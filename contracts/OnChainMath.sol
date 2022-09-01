37d70649627b31f87d80103d3419182e961935c2
ericshenv <wikzfbiy2586@outlook.com>
2022-09-01 20:12:43 +0800

    feat: add basic min and max helpers

--- contracts/OnChainMath.sol
+++ contracts/OnChainMath.sol
@@ -9,5 +9,14 @@ library OnChainMath {
         // (a & b) + (a ^ b) / 2 avoids overflow compared to (a + b) / 2
         return (a & b) + ((a ^ b) >> 1);
     }
-}
 
+    /// @notice Returns the minimum of two unsigned integers.
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    /// @notice Returns the maximum of two unsigned integers.
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a > b ? a : b;
+    }
+}
